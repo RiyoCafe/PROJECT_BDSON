76578021c166d86a49c1cf789611b97fac79d7e4
Riya Dey <1805013@ugrad.cse.buet.ac.bd>
2021-04-19 20:49:44 -0800

    "Splash Screen"

--- app/src/main/java/com/example/watcher/SplashScreen.java
+++ app/src/main/java/com/example/watcher/SplashScreen.java
@@ -3,12 +3,31 @@ package com.example.watcher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
 
+    Animation toanim,bottomanim;
+    ImageView imageView4;
+    TextView textView3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
+
+        toanim= AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomanim=AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+        imageView4=findViewById(R.id.imageView);
+        textView3=findViewById(R.id.textView);
+        imageView4.setAnimation(toanim);
+        textView3.setAnimation(bottomanim);
+
+
+
     }
 }
