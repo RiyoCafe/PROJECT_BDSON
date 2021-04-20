a2c87e2458573a3b56f8ce396ff4ad0ef7187d7c
SanjanaSiraj <sanjanasiraj412@gmail.com>
2021-04-20 17:57:50 +0600

    home page updated

---
+++ app/src/main/java/com/example/watcher/FinalHomeActivity2.java
@@ -0,0 +1,50 @@
+package com.example.watcher;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.widget.TableLayout;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
+public class FinalHomeActivity2 extends AppCompatActivity {
+    TabLayout tabLayout;
+    TabItem tabItem1,tabItem2,tabItem3;
+    ViewPager viewPager;
+    PageAdapter pageAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_final_home2);
+        tabLayout = (TabLayout)findViewById(R.id.tabLayout);
+        tabItem1 = (TabItem)findViewById(R.id.tab1);
+        tabItem2 = (TabItem)findViewById(R.id.tab2);
+        tabItem3 = (TabItem)findViewById(R.id.tab3);
+        viewPager = (ViewPager)findViewById(R.id.vPager);
+        pageAdapter = new PageAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+        viewPager.setAdapter(pageAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+                if (tab.getPosition()==0 || tab.getPosition()==1 || tab.getPosition()==2){
+                    pageAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+    }
+}
